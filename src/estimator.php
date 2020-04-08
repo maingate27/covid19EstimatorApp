b34fa3a13c098a5cd0ab1c797357467e9c1ffde5
muktar6858 <9youngmoney@gmail.com>
2020-04-08 13:13:29 +0100

    first update

--- src/estimator.php
+++ src/estimator.php
@@ -2,5 +2,32 @@
 
 function covid19ImpactEstimator($data)
 {
-  return $data;
+
+  $arr=json_decode($data,true);
+  //echo $arr['region']['name'];
+  
+  #impact
+  
+  $result='{
+    "impact":{
+      "currentlyInfected": 0,
+        "infectionsByRequestedTime": 0
+    },
+    "severeImpact":{
+      "currentlyInfected": 0,
+        "infectionsByRequestedTime": 0
+    },
+    
+  }';
+  
+  $extract=json_decode($result,true);
+  
+  $extract['impact']['currentlyInfected']=$arr['reportedCases'] * 10;
+  $extract['impact']['infectionsByRequestedTime']=$extract['impact']['currentlyInfected'] * 512;
+  
+  $extract['severeImpact']['currentlyInfected']=$arr['reportedCases'] * 50;
+  $extract['severeImpact']['infectionsByRequestedTime']=$extract['severeImpact']['currentlyInfected'] * 512;
+  
+  
+  return $result;
 }
