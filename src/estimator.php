3989561d006ea7be2ccf4b8992d381faa41bef99
muktar6858 <9youngmoney@gmail.com>
2020-04-08 15:20:43 +0100

    re updated

--- src/estimator.php
+++ src/estimator.php
@@ -3,29 +3,7 @@
 function covid19ImpactEstimator($data)
 {
 
-  $arr=json_decode($data,true);
-  //echo $arr['region']['name'];
-  
-  #impact
-  
-  $result='{
-    "impact":{
-      "currentlyInfected": 0,
-        "infectionsByRequestedTime": 0
-    },
-    "severeImpact":{
-      "currentlyInfected": 0,
-        "infectionsByRequestedTime": 0
-    },
-    
-  }';
-  
-  $extract=json_decode($result,true);
-  
-  $extract['impact']['currentlyInfected']=$arr['reportedCases'] * 10;
-  $extract['impact']['infectionsByRequestedTime']=$extract['impact']['currentlyInfected'] * 512;
-  
-  $extract['severeImpact']['currentlyInfected']=$arr['reportedCases'] * 50;
-  $extract['severeImpact']['infectionsByRequestedTime']=$extract['severeImpact']['currentlyInfected'] * 512;
-  return $extract;
+  $return = ['data' => $data, 'impact' => getImpact($data), 'severeImpact' => getSevereImpact($data)];
+    echo print_r($return);
+    return $return;
 }
