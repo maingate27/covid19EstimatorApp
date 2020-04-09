a676958beabbf92d86809f68664a19464db820bd
muktar6858 <9youngmoney@gmail.com>
2020-04-09 11:42:32 +0100

    challenge 1-3

--- src/estimator.php
+++ src/estimator.php
@@ -23,6 +23,16 @@ function getSevereImpact($data)
     $severeImpact = [];
     $severeImpact['currentlyInfected'] = $data['reportedCases'] * 50;
     $severeImpact['infectionsByRequestedTime'] = getInfectionsByRequestedTime($data['periodType'], $data['timeToElapse'], $severeImpact['currentlyInfected']);
+    
+    //challenge 2
+    $severeImpact['severeCasesByRequestedTime'] = 0.15 * $severeImpact['infectionsByRequestedTime'];
+    $severeImpact['hospitalBedsByRequestedTime'] = $data['totalHospitalBeds'] - $severeImpact['severeCasesByRequestedTime'];
+    $severeImpact['hospitalBedsByRequestedTime'] = (0.35 * $data['totalHospitalBeds']) - $severeImpact['severeCasesByRequestedTime'];
+    //challenge 3
+    $severeImpact['casesForICUByRequestedTime'] = floor(0.05 * $severeImpact['infectionsByRequestedTime']);
+    $severeImpact['casesForICUByRequestedTime'] = 0.05 * $severeImpact['infectionsByRequestedTime'];
+    $severeImpact['casesForVentilatorsByRequestedTime'] = 0.02 * $severeImpact['infectionsByRequestedTime'];
+    
     return $severeImpact;
 }
 function getImpact($data)
@@ -30,15 +40,23 @@ function getImpact($data)
     $impact = [];
     $impact['currentlyInfected'] = $data['reportedCases'] * 10;
     $impact['infectionsByRequestedTime'] = getInfectionsByRequestedTime($data['periodType'], $data['timeToElapse'], $impact['currentlyInfected']);
+  
+  //challenge 2
+    $impact['severeCasesByRequestedTime'] = 0.15 * $impact['infectionsByRequestedTime'];
+    $impact['hospitalBedsByRequestedTime'] = $data['totalHospitalBeds'] - $impact['severeCasesByRequestedTime'];
+    $impact['hospitalBedsByRequestedTime'] = (0.35 * $data['totalHospitalBeds']) - $impact['severeCasesByRequestedTime'];
+// challenge 3
+$impact['casesForICUByRequestedTime'] = 0.05 * $impact['infectionsByRequestedTime'];
+
     return $impact;
 }
 
 function covid19ImpactEstimator($data)
 {
 
-  $test= array('data' =>$data ,'impact'=>getImpact($data),'severeImpact'=>getSevereImpact($data) );
+  $result= array('data' =>$data ,'impact'=>getImpact($data),'severeImpact'=>getSevereImpact($data) );
 
-    return $test;
+    return $result;
    
 }
 
