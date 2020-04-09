1e9c4fc4e2677559e924e2569e0d2628b0b4bd87
muktar6858 <9youngmoney@gmail.com>
2020-04-09 10:49:35 +0100

    challenge1

--- src/estimator.php
+++ src/estimator.php
@@ -1,9 +1,38 @@
 <?php
 
+
+function getImpact($data)
+{
+    $impact = new stdClass();
+    $impact->currentlyInfected = $reportedCases * 10;
+    $impact->infectionsByRequestedTime = getInfectionsByRequestedTime($periodType, $timeToElapse, $impact->currentlyInfected);
+    $impact->currentlyInfected = $data->reportedCases * 10;
+    $impact->infectionsByRequestedTime = getInfectionsByRequestedTime($data->periodType, $data->timeToElapse, $impact->currentlyInfected);
+    return $impact;
+}
+
+function getSevereImpact($data)
+{
+    $severeImpact = new stdClass();
+    $severeImpact->currentlyInfected = $reportedCases * 50;
+    $severeImpact->infectionsByRequestedTime = getInfectionsByRequestedTime($periodType, $timeToElapse, $severeImpact->currentlyInfected);
+    $severeImpact->currentlyInfected = $data->reportedCases * 50;
+    $severeImpact->infectionsByRequestedTime = getInfectionsByRequestedTime($data->periodType, $data->timeToElapse, $severeImpact->currentlyInfected);
+    return $severeImpact;
+
+
 function covid19ImpactEstimator($data)
 {
 
-  $return = ['data' => $data, 'impact' => getImpact($data), 'severeImpact' => getSevereImpact($data)];
-    echo print_r($return);
+  $data = (Object)($data);
+    $data1 = (Object)($data);
+    $data = (Array)($data);
+    $return = new stdClass();
+    $return->data = $data;
+    $return->impact = getImpact($data);
+    $return->severeImpact = getSevereImpact($data);
+    $return->impact = getImpact($data1);
+    $return->severeImpact = getSevereImpact($data1);
     return $return;
 }
+
